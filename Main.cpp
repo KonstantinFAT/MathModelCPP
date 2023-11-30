0f381bfb22a04e00ec7e6538e0944574480985c6
KonstantinFAT <45607737+KonstantinFAT@users.noreply.github.com>
2023-11-30 15:22:40 +0300

    Added a splitting method

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>;
-#include <vector>
+#include <vector>;
+#include <cmath>;
+#include <iomanip>;
 
 using namespace std;
 
@@ -14,6 +16,12 @@ public:
 };
 
 int main() {
+	// Константы
+	double D = 0.1;
+	double tay = 0.1;
+	double h = 1;
+	int N = 10;
+
 	// Поле концентраций
 	double X, Y; // Размер поля
 	cin >> X >> Y;
@@ -23,11 +31,21 @@ int main() {
 	double X_source, Y_source, C_source;
 	cin >> X_source >> Y_source >> C_source;
 	concentration_field[X_source][Y_source].C = C_source;
+	
+	// Конечный автомат (в будущем опишу клеточный) (метод расщепления)
+	for (int n = 0; n < N; n++) {
+		for (int i = 1; i < X - 1; i++) {
+			for (int j = 1; j < Y - 1; j++) {
+				concentration_field[i][j].C = concentration_field[i][j].C + D * tay * (concentration_field[i + 1][j].C - 2 * concentration_field[i][j].C + concentration_field[i - 1][j].C) / pow(h, 2);
+				concentration_field[i][j].C = concentration_field[i][j].C + D * tay * (concentration_field[i][j + 1].C - 2 * concentration_field[i][j].C + concentration_field[i][j - 1].C) / pow(h, 2);
+			}
+		}
+	}
 
 	// Вывод
 	for (int i = 0; i < concentration_field.size(); i++) {
 		for (int j = 0; j < concentration_field[i].size(); j++) {
-			cout << concentration_field[i][j].C << " ";;
+			cout << setprecision(2) << setw(7) << concentration_field[i][j].C << " ";;
 		}
 		cout << endl;
 	}
